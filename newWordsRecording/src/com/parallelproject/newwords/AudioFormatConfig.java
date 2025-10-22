d7889455ab42e8c79e10df5b3a8d06c55ce0aba5
sofijasljusar <sofija.sljusar@gmail.com>
2025-10-22 21:44:52 +0300

    Continue Step 2: Implement Consumer thread (+ AudioFormatConfig class)

---
+++ newWordsRecording/src/com/parallelproject/newwords/AudioFormatConfig.java
@@ -0,0 +1,15 @@
+package com.parallelproject.newwords;
+
+import javax.sound.sampled.AudioFormat;
+
+public class AudioFormatConfig {
+    public static final float SAMPLE_RATE = 16000.0f;
+    public static final int SAMPLE_SIZE_BITS = 16;
+    public static final int CHANNELS = 1; // mono
+    public static final boolean SIGNED = true;
+    public static final boolean BIG_ENDIAN = false;
+
+    public static AudioFormat getFormat() {
+        return new AudioFormat(SAMPLE_RATE, SAMPLE_SIZE_BITS, CHANNELS, SIGNED, BIG_ENDIAN);
+    }
+}
